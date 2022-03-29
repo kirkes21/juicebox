a5c8ee9deb1af3cc093201a9b20606cae744f32c
Jaden <jadenshort231@gmail.com>
2022-03-29 13:46:53 -0500

    working on 404 error

--- api/index.js
+++ api/index.js
@@ -1,51 +1,94 @@
-const express = require('express');
-const server = express()
+const express = require("express");
+const server = express();
 const apiRouter = express.Router();
-const usersRouter = require('./users')
-const postsRouter = require('./posts');
-const tagsRouter = require('./tags')
+const usersRouter = require("./users");
+const postsRouter = require("./posts");
+const tagsRouter = require("./tags");
+const bodyParser = require("body-parser");
 
-const jwt = require('jsonwebtoken');
-const { getUserById } = require('../db');
+// Body-parser middleware
+apiRouter.use(bodyParser.urlencoded({ extended: false }));
+apiRouter.use(bodyParser.json());
+
+const jwt = require("jsonwebtoken");
+const { getUserById } = require("../db");
 const { JWT_SECRET } = process.env;
 
 // set `req.user` if possible
 apiRouter.use(async (req, res, next) => {
-    const prefix = 'Bearer ';
-    const auth = req.header('Authorization');
+  const prefix = "Bearer ";
+  const auth = req.header("Authorization");
 
-    if (!auth) { // logged-out state
+  if (!auth) {
+    // nothing to see here
+    next();
+  } else if (auth.startsWith(prefix)) {
+    const token = auth.slice(prefix.length);
+
+    try {
+      const { id } = jwt.verify(token, JWT_SECRET);
+
+      if (id) {
+        req.user = await getUserById(id);
         next();
-    } else if (auth.startsWith(prefix)) {
-        const token = auth.slice(prefix.length);
-
-        try {
-            const { id } = jwt.verify(token, JWT_SECRET);
-
-            if (id) {
-                req.user = await getUserById(id);
-                next();
-            }
-        } catch ({ name, message }) {
-            next({ name, message });
-        }
-    } else {
-        next({
-            name: 'AuthorizationHeaderError',
-            message: `Authorization token must start with ${prefix}`
-        });
+      }
+    } catch ({ name, message }) {
+      next({ name, message });
     }
+  } else {
+    next({
+      name: "AuthorizationHeaderError",
+      message: `Authorization token must start with ${prefix}`,
+    });
+  }
 });
 
-apiRouter.use('/users', usersRouter)
-apiRouter.use('/posts', postsRouter)
-apiRouter.use('/tags', tagsRouter)
+apiRouter.use((req, res, next) => {
+  if (req.user) {
+    console.log("User is set:", req.user);
+  }
+
+  next();
+});
+
+apiRouter.use("/users", usersRouter);
+apiRouter.use("/posts", postsRouter);
+apiRouter.use("/tags", tagsRouter);
+
+server.use(async (req, res, next) => {
+  const prefix = "Bearer ";
+  const auth = req.headers["Authorization"];
+
+  if (!auth) {
+    next(); // don't set req.user, no token was passed in
+  }
+
+  if (auth.startsWith(prefix)) {
+    // recover the token
+    const token = auth.slice(prefix.length);
+    try {
+      // recover the data
+      const { id } = jwt.verify(data, "secret message");
+
+      // get the user from the database
+      const user = await getUserById(id);
+      // note: this might be a user or it might be null depending on if it exists
+
+      // attach the user and move on
+      req.user = user;
+
+      next();
+    } catch (error) {
+      // there are a few types of errors here
+    }
+  }
+});
 
 apiRouter.use((error, req, res, next) => {
-    res.send({
-        name: error.name,
-        message: error.message
-    });
+  res.send({
+    name: error.name,
+    message: error.message,
+  });
 });
 
-module.exports = apiRouter
+module.exports = apiRouter;
--- api/users.js
+++ api/users.js
@@ -1,23 +1,55 @@
-const express = require('express')
-const usersRouter = express.Router()
-const { getAllUsers } = require('../db')
-require('body-parser')
+const express = require("express");
+const usersRouter = express.Router();
+const { getAllUsers, getUserByUsername } = require("../db");
+const jwt = require("jsonwebtoken");
 
 usersRouter.use((req, res, next) => {
-    console.log('A request is being made to /users')
+  console.log("A request is being made to /users");
 
-    next()
-})
+  next();
+});
 
-usersRouter.get('/', async (req, res) => {
-    const users = await getAllUsers()
+usersRouter.get("/", async (req, res) => {
+  const users = await getAllUsers();
 
-    res.send({ users })
-})
+  res.send({ users });
+});
 
-usersRouter.post('/login', async (req, res, next) => {
-    console.log(req.body)
-    res.end()
-})
+usersRouter.post("/login", async (req, res, next) => {
+  const { username, password } = req.body;
 
-module.exports = usersRouter
+  // request must have both
+  if (!username || !password) {
+    next({
+      name: "MissingCredentialsError",
+      message: "Please supply both a username and password",
+    });
+  }
+
+  try {
+    const user = await getUserByUsername(username);
+
+    if (user && user.password == password) {
+      const token = jwt.sign(
+        {
+          id: `"${user.id}"`,
+          username: `"${username}"`,
+        },
+        process.env.JWT_SECRET
+      );
+
+      // create token & return to user
+      res.send({ message: "you're logged in!", key: token });
+    } else {
+      next({
+        name: "IncorrectCredentialsError",
+        message: "Username or password is incorrect",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+module.exports = usersRouter;
