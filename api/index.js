c78701ef085516b45d60e8c1fc614508dcc75a9e
Taylor Kirkes <kirkes21@gmail.com>
2022-03-30 09:32:14 -0500

    fix setting user and create /register

--- api/index.js
+++ api/index.js
@@ -17,10 +17,11 @@ const { JWT_SECRET } = process.env;
 // set `req.user` if possible
 apiRouter.use(async (req, res, next) => {
     const prefix = "Bearer ";
-    const auth = req.headers["Authorization"];
+    const auth = req.header("Authorization");
 
     if (!auth) {
         // nothing to see here
+
         next();
     } else if (auth.startsWith(prefix)) {
         const token = auth.slice(prefix.length);
@@ -54,35 +55,6 @@ apiRouter.use((req, res, next) => {
 apiRouter.use("/users", usersRouter);
 apiRouter.use("/posts", postsRouter);
 apiRouter.use("/tags", tagsRouter);
-
-server.use(async (req, res, next) => {
-    const prefix = "Bearer ";
-    const auth = req.headers["Authorization"];
-
-    if (!auth) {
-        next(); // don't set req.user, no token was passed in
-    }
-
-    if (auth.startsWith(prefix)) {
-        // recover the token
-        const token = auth.slice(prefix.length);
-        try {
-            // recover the data
-            const { id } = jwt.verify(data, "secret message");
-
-            // get the user from the database
-            const user = await getUserById(id);
-            // note: this might be a user or it might be null depending on if it exists
-
-            // attach the user and move on
-            req.user = user;
-
-            next();
-        } catch (error) {
-            // there are a few types of errors here
-        }
-    }
-});
 
 apiRouter.use((error, req, res, next) => {
     res.send({
