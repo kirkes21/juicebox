92a7f29bbb12bdc8bc8be8f34d94afb005e22d00
Taylor Kirkes <kirkes21@gmail.com>
2022-03-30 10:23:56 -0500

    create PATCH and requireUsers for posts

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ server.listen(PORT, () => {
     console.log('The server is up on port', PORT)
 });
 
+server.get('/add/:first/to/:second', (req, res, next) => {
+    res.send(`<h1>${req.params.first} + ${req.params.second} = ${Number(req.params.first) + Number(req.params.second)
+        }</h1>`);
+});
+
 const apiRouter = require('./api')
 server.use('/api', apiRouter)
 
